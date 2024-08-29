d4d2298dae8555a792c5027532dee7a9680e989f
Arham097 <arhamhasan70@gmail.com>
2024-08-29 20:46:23 +0500

    post property updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="982030874605-p2u51qpp4ksmmcpsv9l88lb5mg6bgi1f.apps.googleusercontent.com">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
